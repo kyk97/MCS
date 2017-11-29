08cc1d2900307d1b3f443b0f67b057e69aaa4756
Solodyankin Alexey <kyk_97@mail.ru>
2017-11-30 00:39:26 +0300

    work

--- sort/Source.c
+++ sort/Source.c
@@ -1,179 +1,138 @@
-#include <stdlib.h>
-#include <omp.h>
-#include <assert.h>
-#include <time.h>
-#include <sys/time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <sys/time.h>
+#include <omp.h>
+
+int compare(const void *a, const void *b) {
+     return *(int*)a - *(int*)b;
+ }
 
 
-int compare(const void * x1, const void * x2)
-{
-	return (*(int*)x1 - *(int*)x2);          
-}
+void* merge_sort(int *A, int *B, int left, int right, int m, int threads) {
 
-void swap(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-int binarySearch(int x, int *array, int left, int right) {
-	if (right < left || x <= array[left]) {
-		return left;
-	}
-	if (x > array[right]){
-		return right + 1;
-	}
-	int mid;
-	while (!(left >= right)) {
-		mid = left + (right - left) / 2;
-		if (array[right - 1] < x && array[right] >= x) {
-			return right;
-		}
-		if (array[mid - 1] < x && array[mid] >= x) {
-			return mid;
-		}
-		if (array[mid] < x) {
-			left = mid;
-		}
-		else {
-			right = mid;
-		}
-	}
-	return -1;
-}
-
-void mergeMT(int *T, int left1, int right1, int left2, int right2, int * A, int left3) {
-	int n1 = right1 - left1 + 1;
-	int	n2 = right2 - left2 + 1;
-	if (n1 < n2) {
-		swap(&left1, &left2);
-		swap(&right1, &right2);
-		swap(&n1, &n2);
-	}
-	if (n1 == 0)
-		return;
-	else {
-		int mid1 = (left1 + right1) / 2;
-		int mid2 = binarySearch(T[mid1], T, left2, right2);
-		int mid3 = left3 + (mid1 - left1) + (mid2 - left2);
-		A[mid3] = T[mid1];
-		//#pragma omp parallel
-		{
-			//#pragma omp task
-			mergeMT(T, left1, mid1 - 1, left2, mid2 - 1, A, left3);
-			//#pragma omp task
-			mergeMT(T, mid1 + 1, right1, mid2, right2, A, mid3 + 1);
-		}
-	}
-}
-int* merge_sort(int *up, int *down, int left, int right, int M) {
-
-    int width = right - left;
-    if (width <= M) {
-          qsort(up + left, width, sizeof(int), compare);
-          return up;
+    int width = right - left + 1;
+    if (width <= m) {
+          qsort(A + left, width, sizeof(int), compare);
+          return A;
     }
 
-    int middle = (int)((left + right) * 0.5);
+    int middle = (left + right) / 2;
 
-    int *l_buff;
-    int *r_buff;
+    int *l;
+    int *r;
+    
+     if (threads > 1)
+    {	
+		#pragma omp parallel sections
+		{
+			#pragma omp section
+			{
+				l = (int*)merge_sort(A, B, left, middle, m, threads/2);
+			}
+			#pragma omp section
+			{
+				r = (int*)merge_sort(A, B, middle + 1, right, m, threads - threads/2);
+			}
+		}
+    }
+    else
+    {
+		l = (int*)merge_sort(A, B, left, middle, m, 1);
+		r = (int*)merge_sort(A, B, middle + 1, right, m, 1);
+    }   
 
-    //#pragma omp parallel
-    //{
-	//	#pragma omp single
-    //    {
-    //        #pragma omp task
-    //        {
-                l_buff = merge_sort(up, down, left, middle, M);
-    //        }
-    //        #pragma omp task
-    //        {
-                r_buff = merge_sort(up, down, middle, right, M);
-    //        }
+    int *res = l == A ? B : A;
 
-    //    }
-    //}
+    int l_cur = left, r_cur = middle + 1;
 
+    for (int i = left; i <= right; i++){
 
-    int mid_1 = (middle + left) / 2;
-    int mid_2 = bin_search(r_buff, middle, right, l_buff[mid_1]);
-    int mid_3 = left + (mid_1 - left) + (mid_2  - middle);
+        if (l_cur <= middle && r_cur <= right) {
+            if (l[l_cur] < r[r_cur]){
+                res[i] = l[l_cur];
+                l_cur++;
+            } else {
+                res[i] = r[r_cur];
+                r_cur++;
+                }
+        } else if (l_cur <= middle) {
+            res[i] = l[l_cur];
+            l_cur++;
+        }
+        else {
+            res[i] = r[r_cur];
+            r_cur++;
+        }
 
-    int *target = l_buff == up ? down : up;
-    //#pragma omp parallel
-    //{
-    //    #pragma omp single
-    //    {
-    //        #pragma omp task
-    //        {
-                mergeMT(target, left, l_buff, left, mid_1, r_buff, middle, mid_2);
-
-    //        }
-    //        #pragma omp task
-    //        {
-                mergeMT(target, mid_3, l_buff, mid_1, middle, r_buff, mid_2, right);
-    //        }
-    //    }
-    //}
-
-    return target;
+    }
+ 
+    return res;
 }
 
-void initialization(int *up, int N, int a, int b) {
-	srand(time(NULL));
-	for (int i = 0; i < N; i++) {
-		up[i] = rand() % (b - a + 1) + a;
+int main(int argc, char** argv) {
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    int P = atoi(argv[3]);
+
+    int *A = (int*)malloc(sizeof(int) * n);
+    
+    srand(time(NULL));
+    for(int i = 0; i < n; i++) {
+        A[i] = rand() % (200 + 1);
 	}
-}
 
+    int *B = (int*)malloc(sizeof(int) * n);
+    //int *C = (int*)malloc(sizeof(int) * n);
+    
+    FILE *data = fopen("data.txt", "w");
+    for (int i = 0; i < n; i++) {
+        fprintf(data, "%d ", A[i]);
+    }
+    fprintf(data, "\n");
+    
+    //for (int i = 0; i < n; i++) {
+    //    C[i] = A[i];
+    //}
+    
+    struct timeval start, end;
+    assert(gettimeofday(&start, NULL) == 0);
+    int *res = (int*)merge_sort(A, B, 0, n - 1, m, P);
+    assert(gettimeofday(&end, NULL) == 0);
+    double delta = ((end.tv_sec - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+    
+    for (int i = 0; i < n; i++) {
+        fprintf(data, "%d ", res[i]);
+    }
+    fprintf(data, "\n");    
 
+    FILE *f = fopen("stats.txt", "a");
+    fprintf(f, "%.5f %d %d %d\n", delta, n, m, P);
+    //fprintf(f, "%.5f %d\n", delta, P);
+    
+    //assert(gettimeofday(&start, NULL) == 0);	
+	//qsort(C, n, sizeof(int), compare);
+    //assert(gettimeofday(&end, NULL) == 0);
+    //delta = ((end.tv_sec - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+    //fprintf(f,"%0.5f\n", delta);
+	//int flag = 1;
+	//for (int i = 0; i < n; i++)
+	//	if (res[i] != C[i]){
+	//		flag = 0;
+	//		fprintf(data, "\n%d %d %d\n", i, A[i], C[i]);
+	//	}
+	//fprintf(f,"%d\n", flag);	
+    //for (int i = 0; i < n; i++) {
+	//	fprintf(data, "%d ", C[i]);
+    //}
+    fprintf(data, "\n");    
+    fclose(data);
+    
+    fclose(f);
+    free(A);
+    free(B);
 
-int main(int argc, char **argv) 
-{
-	if (argc == 4){
-		int n = atoi(argv[1]);
-		int m = atoi(argv[2]);
-		int P = atoi(argv[3]);
-		FILE *f = fopen("stats.txt", "a");
-
-		omp_set_num_threads(P);
-		int * a = (int*) malloc(n * sizeof(int));
-		int * b = (int*) malloc(n * sizeof(int));
-	
-		initialization(a, n, 0, 100);
-
-
-		struct timeval start, end;
-		assert(gettimeofday(&start, NULL) == 0);
-		int *res = merge_sort(a, b, 0, n, m);
-		assert(gettimeofday(&end, NULL) == 0);
-		double delta = ((end.tv_sec - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
-
-		fprintf(f, "%.5f %d %d %d\n", delta, n, m, P);
-
-		FILE *data = fopen("data.txt", "w");
-		fprintf(data, "\n");
-		for (int i = 0; i < n; i++) {
-			fprintf(data, "%d ", res[i]);
-		}		
-		
-		
-		assert(gettimeofday(&start, NULL) == 0);
-		qsort(a, n, sizeof(int), compare);
-		assert(gettimeofday(&end, NULL) == 0);
-		delta = ((end.tv_sec - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
-		fprintf(f, "%f \n", delta);
-		
-		fprintf(data, "\n");
-		for (int i = 0; i < n; i++) {
-			fprintf(data, "%d ", a[i]);
-		}		
-		fclose(data);
-		fclose(f);
-		free(a);
-		free(b);
-	}
-	return 0;
+    //free(C);
+    return 0;
 }
